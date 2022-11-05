62d0e535043b130f133fb7fc0568117bc25c9afa
leovingo <89054845+leovingo@users.noreply.github.com>
2022-11-05 11:50:47 +0700

    Update CommandLineHandling.cpp

--- CommandLineHandling.cpp
+++ CommandLineHandling.cpp
@@ -52,56 +52,114 @@ void Command_line(int argv, char* argv2[]) {
 			return;
 	}
 }
-void Command_line_1(char* argv[]){}
+void Command_line_1(char* argv[]) {
+
+    //tính size
+    int size = 0;
+    int* arr;
+    // gán biến đầu ra
+    int output;
+    if (strcmp(argv[4], "-time") == 0) {
+        output = 0;
+    }
+    else if (strcmp(argv[4], "-comp") == 0) {
+        output = 1;
+    }
+    else if (strcmp(argv[4], "-both") == 0) {
+        output = 2;
+    }
+    else {
+        output = -1;
+    }
+    ifstream fin;
+    fin.open(argv[3]);
+    if (fin.is_open()) {
+        fin >> size;
+        arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            fin >> arr[i];
+        }
+        fin.close();
+    }
+    else {
+        cout << "Error!" << endl;
+        system("pause");
+        return;
+    }
+    //vì kích thước mảng có thể lớn nên khuyên mọi người cấp phát động
+    double time1;
+    int comp1;
+    comp1 = Calculate_Complexity(argv, 2, time1, arr, size);
+    cout << "ALGORITHM MODE\n";
+    cout << "Algorithm:" << argv[2] << endl;
+    cout << "Input file: " << argv[3] << endl;
+    cout << "Input size: " << size << endl;
+    cout << "--------------\n";
+   switch (output)
+    {
+    case 0:
+        cout << "Running time: " << fixed << time1 << setprecision(3) << " msec " << endl;
+        break;
+    case 1:
+        cout << "Comparisions: " << comp1 << endl;
+        break;
+    case 2:
+        cout << "Running time: " << fixed << time1 << setprecision(3) << " msec " << endl;
+        cout << "Comparisions: " << comp1 << endl;
+        break;
+    default:
+        break;
+    }
+    cout << endl;
+}
 void Command_line_2(char* argv[]) {
 
-	//tính size
-	int size = atoi(argv[4]);
-	int* arr = new int[size];
-	string order;
-	GenerateData(arr, size, data_order(argv, 5, order));
-	// gán biến đầu ra
-	int output;
-	if (strcmp(argv[4], "-time") == 0) {
-		output = 0;
-	}
-	else if (strcmp(argv[4], "-comp") == 0) {
-		output = 1;
-	}
-	else if (strcmp(argv[4], "-both") == 0) {
-		output = 2;
-	}
-	else {
-		output = -1;
-	}
+    //tính size
+    int size = atoi(argv[3]);
+    int* arr = new int[size];
+    string order;
+    GenerateData(arr, size, data_order(argv, 4, order));
+    // gán biến đầu ra
+    int output;
+    if (strcmp(argv[5], "-time") == 0) {
+        output = 0;
+    }
+    else if (strcmp(argv[5], "-comp") == 0) {
+        output = 1;
+    }
+    else if (strcmp(argv[5], "-both") == 0) {
+        output = 2;
+    }
+    else {
+        output = -1;
+    }
 
-	//vì kích thước mảng có thể lớn nên khuyên mọi người cấp phát động
+    //vì kích thước mảng có thể lớn nên khuyên mọi người cấp phát động
 
-	double time1;
-	int comp1;
-	comp1 = Calculate_Complexity(argv, 2, time1, arr, size);
-	cout << "ALGORITHM MODE\n";
-	cout << "Algorithm:" << argv[2] << endl;
-	cout << "Input size: " << argv[4] << endl;
-	cout << "Input order: " << order << endl;
-	GenerateData(arr, size, 0);
-	cout << "--------------\n";
-	switch (output)
-	{
-	case 0:
-		cout << "Running time: " << fixed << time1 << setprecision(3) << " msec " << endl;
-		break;
-	case 1:
-		cout << "Comparisions: " << comp1 << endl;
-		break;
-	case 2:
-		cout << "Running time: " << fixed << time1 << setprecision(3) << " msec " << endl;
-		cout << "Comparisions: " << comp1 << endl;
-		break;
-	default:
-		break;
-	}
-	cout << endl;
+    double time1;
+    int comp1;
+    comp1 = Calculate_Complexity(argv, 2, time1, arr, size);
+    cout << "ALGORITHM MODE\n";
+    cout << "Algorithm:" << argv[2] << endl;
+    cout << "Input size: " << argv[3] << endl;
+    cout << "Input order: " << order << endl;
+    cout << "--------------\n";
+    switch (output)
+    {
+    case 0:
+        cout << "Running time: " << fixed << time1 << setprecision(3) << " msec " << endl;
+        break;
+    case 1:
+        cout << "Comparisions: " << comp1 << endl;
+        break;
+    case 2:
+        cout << "Running time: " << fixed << time1 << setprecision(3) << " msec " << endl;
+        cout << "Comparisions: " << comp1 << endl;
+        break;
+    default:
+        break;
+    }
+    cout << endl;
 }
 void Command_line_3(char* argv[]) {
 	//tính size
